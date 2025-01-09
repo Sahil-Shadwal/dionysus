a9fb3b2c0b8b5bcf98672b8e6ac5108bffaae242
Sahil-Shadwal <sahilshadwal@gmail.com>
2025-01-09 22:28:25 +0530

    installed whole frekign shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "~/components/ui/button";
+
 export default async function Home() {
   return (
     <div>
       <div className="-z-10 flex h-screen items-center justify-center bg-orange-200">
-        <h1 className="text-3xl">Hello World</h1>
+        <div className="text-center">
+          <h1 className="mb-4 text-3xl">Hello World</h1>
+          <Button className="px-5">Click away!</Button>
+        </div>
       </div>
     </div>
   );

63aaa776e9cc43c38c15863a928b370dcf285990
Sahil-Shadwal <sahilshadwal@gmail.com>
2025-01-09 22:11:37 +0530

    hello world

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div>
+      <div className="-z-10 flex h-screen items-center justify-center bg-orange-200">
+        <h1 className="text-3xl">Hello World</h1>
+      </div>
+    </div>
+  );
+}
